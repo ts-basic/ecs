83caf795d5518fd265c4ae34183e2b893959ebe5
Benjamin Neal Gearig <contact@bengearig.com>
2023-04-16 15:37:05 -0400

    extract interface IPerformsUpdates from BaseSystem

--- packages/ecs/src/BaseSystem.ts
+++ packages/ecs/src/BaseSystem.ts
@@ -1,10 +1,22 @@
 import { AbstractConstructor } from "./types";
 import { ECS } from "./ECS";
 
-export abstract class BaseSystem<T extends AbstractConstructor = AbstractConstructor> {
+export interface IPerformsUpdates {
+    onEarlyUpdate(deltaTime: number): void;
+
+    onUpdate(deltaTime: number): void;
+
+    onLateUpdate(deltaTime: number): void;
+}
+
+export abstract class BaseSystem<T extends AbstractConstructor = AbstractConstructor>
+    implements IPerformsUpdates
+{
     constructor(public ecs: ECS<T>) {}
 
     public abstract onEarlyUpdate(deltaTime: number): void;
+
     public abstract onUpdate(deltaTime: number): void;
+
     public abstract onLateUpdate(deltaTime: number): void;
 }
